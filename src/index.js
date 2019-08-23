5f34e6fededffd36c754654f4e1eaf226ba256c1
jjinyong.lee <jjinyong.lee@samsung.com>
2019-08-23 15:11:42 +0900

    Add user db & room message

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
 import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
 const server = http.createServer(app);
@@ -11,26 +12,76 @@ const io = socketIO(server, {
   pingTimeout: 5000,
   cookie: false,
 });
+app.use(express.urlencoded());
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.engine('ejs', ejs.__express);
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'admin',
+  password: 'adminpassword',
+  database: 'chatchat'
+});
+
+pool.getConnection(function (err, connection) {
+  if (err) throw err;
+  var sql = `CREATE TABLE if not exists users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    primary key (id)
+    )`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log('USER Table created');
+  });
+  connection.release();
+});
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../client.html'));
+  res.render('login');
 });
-app.use(express.static('node_modules'));
 
-var count = 1;
+app.post('/login', function (req, res) {
+
+  const { body } = req;
+  const { username } = body;
+
+  pool.getConnection(function (err, connection) {
+    if (err) throw err;
+    var sql = `INSERT INTO users (name) VALUES ('${username}')`;
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log('user is inserted');
+    });
+    connection.release();
+  });
+
+  res.render('chat', {
+    username: username
+  });
+});
+
 io.on('connection', function (socket) {
   console.log('user connected: ', socket.id);
-  var name = "user" + count++;
-  io.to(socket.id).emit('change name', name);
 
   socket.on('disconnect', function () {
     console.log('user disconnected: ', socket.id);
   });
 
-  socket.on('send message', function (name, text) {
-    var msg = name + ' : ' + text;
-    console.log(msg);
+  socket.on('join', function (room) {
+    socket.join(room);
+  });
+
+  socket.on('send broadcast message', function (username, text) {
+    var msg = `${username} : ${text}`;
     io.emit('receive message', msg);
+  });
+  socket.on('send room message', function (username, text, receiver) {
+    var msg = `${username} : ${text}`;
+    io.to(username).to(receiver).emit('receive room message', msg, room);
   });
 });
 
